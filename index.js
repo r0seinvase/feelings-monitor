40c7363c71e7d42c962d7297ee53a31dc110ce7c
wael <waeelash155@gmail.com>
2022-02-16 14:43:50 -0500

    style updates.2

--- index.js
+++ index.js
@@ -1,36 +1,39 @@
 // get relevant emojis from emoji-API
+const emojiReference = {};
 
-const angryFace = fetch('https://emoji-api.com/emojis/angry-face?access_key=8bb25ebd0a3a427ee5afe3f9222517ed85c84920')
+emojiReference.angry = fetch('https://emoji-api.com/emojis/angry-face?access_key=8bb25ebd0a3a427ee5afe3f9222517ed85c84920')
     .then(response => response.json())
     .then(data => data[0].character);
 
-const happyFace = fetch('https://emoji-api.com/emojis/grinning-face-with-big-eyes?access_key=8bb25ebd0a3a427ee5afe3f9222517ed85c84920')
+emojiReference.happy = fetch('https://emoji-api.com/emojis/grinning-face-with-big-eyes?access_key=8bb25ebd0a3a427ee5afe3f9222517ed85c84920')
     .then(response => response.json())
     .then(data => data[0].character);
 
-const disgustedFace = fetch('https://emoji-api.com/emojis/face-vomiting?access_key=8bb25ebd0a3a427ee5afe3f9222517ed85c84920')
+emojiReference.disgusted = fetch('https://emoji-api.com/emojis/face-vomiting?access_key=8bb25ebd0a3a427ee5afe3f9222517ed85c84920')
     .then(response => response.json())
     .then(data => data[0].character);
 
-const sadFace = fetch('https://emoji-api.com/emojis/crying-face?access_key=8bb25ebd0a3a427ee5afe3f9222517ed85c84920')
+emojiReference.sad = fetch('https://emoji-api.com/emojis/crying-face?access_key=8bb25ebd0a3a427ee5afe3f9222517ed85c84920')
     .then(response => response.json())
     .then(data => data[0].character);
 
-const fearfulFace = fetch('https://emoji-api.com/emojis/anxious-face-with-sweat?access_key=8bb25ebd0a3a427ee5afe3f9222517ed85c84920')
+emojiReference.fearful = fetch('https://emoji-api.com/emojis/anxious-face-with-sweat?access_key=8bb25ebd0a3a427ee5afe3f9222517ed85c84920')
     .then(response => response.json())
     .then(data => data[0].character);
 
-const surprisedFace = fetch('https://emoji-api.com/emojis/astonished-face?access_key=8bb25ebd0a3a427ee5afe3f9222517ed85c84920')
+emojiReference.surprised = fetch('https://emoji-api.com/emojis/astonished-face?access_key=8bb25ebd0a3a427ee5afe3f9222517ed85c84920')
     .then(response => response.json())
     .then(data => data[0].character);
 
-const excitedFace = fetch('https://emoji-api.com/emojis/star-struck?access_key=8bb25ebd0a3a427ee5afe3f9222517ed85c84920')
+emojiReference.excited = fetch('https://emoji-api.com/emojis/star-struck?access_key=8bb25ebd0a3a427ee5afe3f9222517ed85c84920')
     .then(response => response.json())
     .then(data => data[0].character);
 
-const neutralFace = fetch('https://emoji-api.com/emojis/face-without-mouth?access_key=8bb25ebd0a3a427ee5afe3f9222517ed85c84920')
+neutralFace = fetch('https://emoji-api.com/emojis/face-without-mouth?access_key=8bb25ebd0a3a427ee5afe3f9222517ed85c84920')
     .then(response => response.json())
     .then(data => data[0].character);
+
+
 
 // had some error with the cloud url, i think    
 // const cloudFace = fetch('https://emoji-api.com/emojis/face-in-clouds?access_key=8bb25ebd0a3a427ee5afe3f9222517ed85c84920')
@@ -51,47 +54,43 @@ const populateEmojiSpace = async (face) => {
   
 populateEmojiSpace(neutralFace);
 
-// create event listeners for feelings selection
 
-const happyButton = document.getElementById('happy');
-const angryButton = document.getElementById('angry');
-const sadButton = document.getElementById('sad');
-const disgustedButton = document.getElementById('disgusted');
-const surprisedButton = document.getElementById('surprised');
-const excitedButton = document.getElementById('excited');
-const fearfulButton = document.getElementById('fearful');
+const emotionButtons = document.querySelectorAll('.feelings-button');
+emotionButtons.forEach(emotionButton => {
+    emotionButton.addEventListener('change', () => {
+        populateEmojiSpace(emojiReference[emotionButton.value]);
+    })
+    emotionButton.addEventListener('mouseover', () => {
+        populateEmojiSpace(emojiReference[emotionButton.value]);
+    })
+})
 
-happyButton.addEventListener('change', () => {
-    console.log('i selected happy');
-    populateEmojiSpace(happyFace);
-});
 
-angryButton.addEventListener('change', () => {
-    console.log('i selected angry');
-    populateEmojiSpace(angryFace);
-});
+const feelingsForm = document.getElementById('feelings-form')
 
-sadButton.addEventListener('change', () => {
-    console.log('i selected sad');
-    populateEmojiSpace(sadFace);
-});
+const makeCard = async (face, comment) => {
+    const container = document.createElement('div')
+    const ventingSubmit = document.createElement('p')
+    const surpriseNode =document.createElement('p')
+    const a = await emojiReference[face];
+    
+    surpriseNode.textContent = a;
+        ventingSubmit.textContent = comment
+    container.appendChild(ventingSubmit)
+            container.appendChild(surpriseNode)
+            document.body.appendChild(container)
 
-disgusted.addEventListener('change', () => {
-    console.log('i selected disgusted');
-    populateEmojiSpace(disgustedFace);
-});
+};
 
-surprisedButton.addEventListener('change', () => {
-    console.log('i selected surprised');
-    populateEmojiSpace(surprisedFace);
-});
+feelingsForm.addEventListener('submit', function(e){
+    e.preventDefault()
 
-excitedButton.addEventListener('change', () => {
-    console.log('i selected excited');
-    populateEmojiSpace(excitedFace);
-});
+    const ventingInput = document.getElementById('venting').value
 
-fearfulButton.addEventListener('change', () => {
-    console.log('i selected fearful');
-    populateEmojiSpace(fearfulFace);
-});
+
+    const alongFace = document.querySelector('input[name="feeling"]:checked').value;
+
+makeCard(alongFace, ventingInput)
+e.target.reset()
+
+})
